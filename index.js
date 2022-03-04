3ace8ce7f7533c5511a7ebcf97835c31dd6c147a
Symone Varnado <symonevarnado@gmail.com>
2022-03-04 14:38:36 -0600

    got graphl working

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { deburr } = require("lodash");
+const models = require('./models')
+
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-  type Query {
-    hello: String
+type User {
+    firstName: String!
+    lastName:String!
+    email: String!
+  }
+type Query {
+    users:[User]
+  }
+type Mutation {
+    createUser(firstName: String!,lastName: String!,email: String!): User!
   }
 `;
 
+const users = [{firstName:"Symone", lastName:"Varnado", email:"symonevarnado@gmai.com"}]
+
 // Provide resolver functions for your schema fields
+let db = models
+console.log(db.User, "checking")
 const resolvers = {
+    
   Query: {
-    hello: (root, args, context) => "Hello world!"
-  }
+    async users(root, { email }, models ) {
+        return db.User.findAll();
+      },
+  },
+  Mutation: {
+    async createUser(root, {firstName, lastName, email }, models) {
+      const newUser = await  db.User.create({
+        firstName,
+        lastName,
+        email
+      });
+
+      return newUser
+    },
+}
 };
+
+
 
 const server = new ApolloServer({
   typeDefs,
